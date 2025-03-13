83928df0592e0d4a2d40c18698458395932a8c0f
EleqfulCEAC <elean.quintero@alu.ceacfp.es>
2025-03-13 13:49:54 +0100

    añadiendo sintaxis de bucles

--- app.py
+++ app.py
@@ -1,4 +1,48 @@
-var = "hola"    
-print(var)
-var = "adios"
-print(var)
+"""var = "hola"    
+var2 = "adios"
+var3 = "hasta luego"
+var4 = "hasta pronto"
+print(var, var2, var3, var4, end="++",sep="?")"""
+
+
+#Ejercicio 1
+def calculo(text):
+    result = int(input(text))
+    return result
+
+"""base = calculo("Introduce la base")
+altura = calculo("Introduce la altura")
+area = base*altura/2
+print(area)"""
+
+#Ejercicio 2
+"""celcius = calculo("Introduce la temperatura en celcius")
+def toFarenheit(weather):
+    farenheit = (weather * 9 ) / 5 + 32
+    return farenheit
+
+print(toFarenheit(celcius))"""
+
+#Ejercicio 3
+"""radio = calculo("dame el radio de tu circulo")
+def getPerimetro(radio):
+    perimetro = (3.14 * 2) * radio
+    return perimetro
+
+perimetro = round(getPerimetro(radio))
+print(perimetro)
+
+def getArea(radio):
+    area = 3.14 * radio**2
+    return area
+
+area = getArea(radio)
+print(area)"""
+
+
+#Ejercicio 4
+numero = calculo("introduce el numero")
+result = numero % 2
+if ( result == 0):
+    print("Es par")
+else: print("Es impar")
---
+++ bucles.py
@@ -0,0 +1,7 @@
+#Opciones de bucles for
+var = "hola bebe"
+for i in range(len(var)):
+    print(var[i])
+    
+for i,v in enumerate(var):
+    print(i,v)
