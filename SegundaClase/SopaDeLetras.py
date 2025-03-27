52169ac7d3010d3186e01856af91d57674cce0da
EleqfulCEAC <elean.quintero@alu.ceacfp.es>
2025-03-27 14:13:07 +0100

    sopa de letras

---
+++ SegundaClase/SopaDeLetras.py
@@ -0,0 +1,19 @@
+sopaLetras = [
+  ['Y', 'W', 'D', 'E', 'N', 'V', 'U', 'D', 'M', 'R'],
+  ['A', 'Q', 'C', 'V', 'U', 'J', 'Y', 'O', 'A', 'D'],
+  ['O', 'L', 'W', 'T', 'N', 'M', 'G', 'M', 'Z', 'J'],
+  ['W', 'E', 'G', 'T', 'G', 'I', 'A', 'P', 'R', 'V'],
+  ['F', 'G', 'N', 'U', 'D', 'R', 'X', 'L', 'L', 'B'],
+  ['Q', 'J', 'B', 'O', 'R', 'K', 'W', 'X', 'N', 'R'],
+  ['K', 'E', 'C', 'O', 'B', 'I', 'I', 'U', 'A', 'I'],
+  ['D', 'F', 'R', 'S', 'O', 'L', 'T', 'W', 'W', 'I'],
+  ['B', 'P', 'S', 'V', 'P', 'L', 'J', 'M', 'U', 'G'],
+  ['K', 'T', 'S', 'V', 'Z', 'O', 'C', 'H', 'O', 'D']]
+# Palabras a buscar
+palabras = ['PROGRAMAR', 'ALGORITMO', 'DEBUG', 'CODIGO']
+direcciones=[(0,1),(0,-1),(1,0),(-1,0),(1,1),(-1,1),(-1,-1),(1,-1)]
+
+# Bucle que recorre todo el tablero
+'''for x in sopaLetras:
+    for y in x:'''
+       
